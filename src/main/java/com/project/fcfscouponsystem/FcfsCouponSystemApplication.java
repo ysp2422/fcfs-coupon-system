0cd039c57da5d8070eb9c047c5d5fbfc996daddc
YuSeongPyo <yuseongpyo@YuSeongPyoui-MacBookAir.local>
2026-03-26 16:53:21 +0900

    초기 설정: 프로젝트 세팅

---
+++ src/main/java/com/project/fcfscouponsystem/FcfsCouponSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.fcfscouponsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcfsCouponSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcfsCouponSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/fcfscouponsystem/FcfsCouponSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fcfscouponsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcfsCouponSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
